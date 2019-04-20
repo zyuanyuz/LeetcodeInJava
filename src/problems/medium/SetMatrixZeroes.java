48ef9e1077eed85ead231e7ce211dcc10cd70ab5
zyuanyuz <zyuanyuz@qq.com>
2019-04-20 09:46:43 +0800

    leetcode 73 SetMatrixZeros Medium
    Runtime: 1 ms, faster than 95.32% of Java online submissions for Set Matrix Zeroes.
    Memory Usage: 45.5 MB, less than 52.55% of Java online submissions for Set Matrix Zeroes.

--- src/problems/medium/SetMatrixZeroes.java
+++ src/problems/medium/SetMatrixZeroes.java
@@ -1,7 +1,7 @@
 package problems.medium;
 
 /**
- * leetcode 73 SetMatrixZeros
+ * leetcode 73 SetMatrixZeros Medium
  * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
  * <p>
  * Example 1:
@@ -39,14 +39,54 @@ package problems.medium;
  * Could you devise a constant space solution?
  */
 public class SetMatrixZeroes {
-    //
+    //Runtime: 1 ms, faster than 95.32% of Java online submissions for Set Matrix Zeroes.
+    //Memory Usage: 45.5 MB, less than 52.55% of Java online submissions for Set Matrix Zeroes.
     public void setZeroes(int[][] matrix) {
-
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        boolean isFirstRowZero = false, isFirstColZero = false;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                isFirstColZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < matrix[0].length; j++) matrix[i][j] = 0;
+            }
+        }
+        for (int i = 1; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 0; j < matrix.length; j++) matrix[j][i] = 0;
+            }
+        }
+        if (isFirstRowZero) {
+            for (int i = 0; i < matrix[0].length; i++) matrix[0][i] = 0;
+        }
+        if (isFirstColZero) {
+            for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+        }
     }
 
     public void test() {
         //{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}
-        //
+        //[[1,1,1],[1,0,1],[1,1,1]]
         int[][] matrix = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
         setZeroes(matrix);
         for (int[] row : matrix) {
