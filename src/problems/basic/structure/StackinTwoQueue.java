890f7f119fa250e43c4e0216af5d8948b81e66db
zyuanyuz <zyuanyuz@qq.com>
2019-05-05 09:48:01 +0800

    StackinTwoQueue
    Implementing the stack structure using two queues

--- src/problems/basic/structure/StackinTwoQueue.java
+++ src/problems/basic/structure/StackinTwoQueue.java
@@ -1,6 +1,41 @@
 package problems.basic.structure;
+
 import java.util.*;
+
 /**
+ * StackinTwoQueue 
  * 
+ * Implementing the stack structure using two queues
  */
-public class 
+public class StackinTwoQueue<T> extends Stack<T> {
+    Queue<T> queueOne = new LinkedList<>();
+    Queue<T> queueTwo = new LinkedList<>();
+    boolean queueSwitch = true;
+
+    public T push(T item) {
+        // queueTake only contains one newest item for func pop
+        Queue<T> queueTake = queueSwitch ? queueOne : queueTwo;
+        Queue<T> queueStorage = queueSwitch ? queueTwo : queueOne;
+        if (queueTake.isEmpty()) {
+            queueTake.offer(item);
+        } else {
+            queueStorage.offer(queueTake.poll());
+            queueTake.offer(item);
+        }
+        return item;
+    }
+
+    public T pop() {
+        Queue<T> queueTake = queueSwitch ? queueOne : queueTwo;
+        Queue<T> queueStorage = queueSwitch ? queueTwo : queueOne;
+        if(queueTake.isEmpty()){
+            return null;
+        }
+        T item = queueTake.poll();
+        while(queueStorage.size()>1){
+            queueTake.offer(queueStorage.poll());
+        }
+        queueSwitch = !queueSwitch;
+        return item;
+    }
+}
