0a5a3857130b6c3eff1aab728048fd55793622df
zyuanyuz <zyuanyuz@qq.com>
2019-05-07 16:55:22 +0800

    leetcode 144 BinaryTreePreorderTraversal Medium
    Runtime: 1 ms, faster than 35.79% of Java online submissions for Binary Tree Preorder Traversal.
    Memory Usage: 33.9 MB, less than 93.91% of Java online submissions for Binary Tree Preorder Traversal.

---
+++ src/problems/medium/BinaryTreePreorderTraversal.java
@@ -0,0 +1,48 @@
+package problems.medium;
+
+import java.util.*;
+
+/**
+ * leetcode 144 BinaryTreePreorderTraversal Medium
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+public class BinaryTreePreorderTraversal {
+    // Runtime: 1 ms, faster than 35.79% of Java online submissions for Binary Tree
+    // Preorder Traversal.
+    // Memory Usage: 33.9 MB, less than 93.91% of Java online submissions for Binary
+    // Tree Preorder Traversal.
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null)
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> nodeStack = new Stack<>();
+        result.add(root.val);
+        nodeStack.push(root);
+        while (!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.peek();
+            if (node.left != null) {
+                result.add(node.left.val);
+                nodeStack.push(node.left);
+                node.left = null;
+            } else if (node.right != null) {
+                result.add(node.right.val);
+                nodeStack.push(node.right);
+                node.right = null;
+            } else {
+                nodeStack.pop();
+            }
+        }
+        return result;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
--- src/problems/medium/PalindromePartitioning.java
+++ src/problems/medium/PalindromePartitioning.java
@@ -9,12 +9,39 @@ import java.util.*;
  * palindrome.
  * 
  * Return all possible palindrome partitioning of s.
+ * 
+ * See leetcode 5
  */
 public class PalindromePartitioning {
     //
     //
     public List<List<String>> partition(String s) {
+        if (s == null || s.length() == 0) {
+            return new ArrayList<>();
+        }
+        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
+        for (int length = 1; length <= s.length(); length++) {
+            for (int i = 0; i < s.length() - length + 1; i++) {
+                if (length == 1)
+                    isPalindrome[i][i + length - 1] = true;
+                if (length == 2)
+                    isPalindrome[i][i + length - 1] = (s.charAt(i) == s.charAt(i + length - 1));
+                else
+                    isPalindrome[i][i + length - 1] = isPalindrome[i + 1][i + length - 2];
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        List<String> tmpList = new ArrayList<>();
 
         return null;
     }
+
+    void backtracking(List<List<String>> result,List<String> tmpList,boolean[][] isPalin,int index,String s){
+        if(index == s.length()){
+            
+        }else{ 
+
+        }
+    }
+
 }
