7e74fa19555b049eb4eb8a147f70041dce675a5a
zyuanyuz <zyuanyuz@outlook.com>
2019-08-04 22:52:20 +0800

    leetcode 107 BinaryTreeLevelOrderTraversalII Easy

--- src/problems/easy/BinaryTreeLevelOrderTraversalII.java
+++ src/problems/easy/BinaryTreeLevelOrderTraversalII.java
@@ -4,16 +4,32 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
- * leetcode 107 
+ * leetcode 107 BinaryTreeLevelOrderTraversalII Easy
  */
 
 public class BinaryTreeLevelOrderTraversalII {
+    //Runtime: 1 ms, faster than 97.85% of Java online submissions for Binary Tree Level Order Traversal II.
+    //Memory Usage: 36.5 MB, less than 100.00% of Java online submissions for Binary Tree Level Order Traversal II.
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        if(root == null){
-            return new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
         }
-        
-        return null;
+        makeupList(result, root, 0);
+        return result;
+    }
+
+    public void makeupList(List<List<Integer>> result, TreeNode node, int deepth) {
+        if (result.size() < deepth + 1) {
+            result.add(0, new ArrayList<>());
+        }
+        result.get(result.size() - deepth - 1).add(node.val);
+        if (node.left != null) {
+            makeupList(result, node.left, deepth + 1);
+        }
+        if (node.right != null) {
+            makeupList(result, node.right, deepth + 1);
+        }
     }
 
     class TreeNode {
