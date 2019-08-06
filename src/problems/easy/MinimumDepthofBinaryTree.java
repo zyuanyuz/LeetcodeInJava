4e1f97d7feb8eef128a652ea576784fb76b0a99c
zyuanyuz <zyuanyuz@outlook.com>
2019-08-06 21:28:22 +0800

    leetcode 111 MinimumDepthofBinaryTree Easy
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Depth of Binary Tree.
    Memory Usage: 38.7 MB, less than 97.79% of Java online submissions for Minimum Depth of Binary Tree.

--- src/problems/easy/MinimumDepthofBinaryTree.java
+++ src/problems/easy/MinimumDepthofBinaryTree.java
@@ -1,17 +1,32 @@
 package problems.easy;
 
 /**
- * leetcode 111
+ * leetcode 111 MinimumDepthofBinaryTree Easy
  */
 public class MinimumDepthofBinaryTree {
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Depth of Binary Tree.
+    //Memory Usage: 38.7 MB, less than 97.79% of Java online submissions for Minimum Depth of Binary Tree.
+    int minDeepth = Integer.MAX_VALUE;
 
-    int result = Integer.MAX_VALUE;
     public int minDepth(TreeNode root) {
-        
+        if (root == null) {
+            return 0;
+        }
+        getMinDeepth(root, 0);
+        return minDeepth;
     }
 
-    int getMinDeepth(TreeNode node,int deepth){
-        
+    void getMinDeepth(TreeNode node, int deepth) {
+        if (node.left == null && node.right == null) {
+            if (deepth + 1 < minDeepth)
+                minDeepth = ++deepth;
+        }
+        if (node.left != null) {
+            getMinDeepth(node.left, deepth + 1);
+        }
+        if (node.right != null) {
+            getMinDeepth(node.right, deepth + 1);
+        }
     }
 
     class TreeNode {
