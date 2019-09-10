3d4b88732c73894c0580c811042a2d3b8828d660
zyuanyuz <zyuanyuz@outlook.com>
2019-09-10 22:52:58 +0800

    onw step

---
+++ src/problems/medium/WordBreak.java
@@ -0,0 +1,21 @@
+package problems.medium;
+
+import java.util.*;
+
+/**
+ * leetcode 139
+ */
+public class WordBreak {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+    }
+
+    boolean makeUpStr(String s,int index,List<String> wordDict){
+        if(index==s.length()){
+            return true;
+        }else if(index<s.length()){
+            
+        }
+    }
+}
