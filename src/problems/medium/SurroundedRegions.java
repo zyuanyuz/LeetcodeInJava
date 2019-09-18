82c9bd66d82437f091acb9772174284377b99c08
zyuanyuz <zyuanyuz@outlook.com>
2019-09-18 22:04:36 +0800

    onw step

--- src/problems/medium/SurroundedRegions.java
+++ src/problems/medium/SurroundedRegions.java
@@ -1,13 +1,25 @@
 package problems.medium;
 
 /**
- * leetcode 130 SurroundedRegions
+ * leetcode 130
  * 
  */
 
 public class SurroundedRegions {
-    
+
     public void solve(char[][] board) {
-        
+        if (board.length == 0) {
+            return;
+        }
+        byte[][] isVisited = new byte[board.length][board[0].length];
+        for (int i = 0; i < isVisited.length; i++) {
+            for (int j = 0; j < isVisited[0].length; j++) {
+                if (board[i][j] == 'O' && isVisited[i][j] == 0) {
+
+                }
+            }
+        }
     }
+
+    
 }
