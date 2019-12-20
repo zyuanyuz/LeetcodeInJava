76179d955a498d47f2fa3a9547adc917acbcbd42
zyuanyuz <zyuanyuz@outlook.com>
2019-12-20 18:56:56 +0800

    leetcode 25 hard ReverseNodesinkGroup

--- src/problems/hard/ReverseNodesinkGroup.java
+++ src/problems/hard/ReverseNodesinkGroup.java
@@ -1,7 +1,7 @@
 package problems.hard;
 
 /**
- * leetcode 25 hard
+ * leetcode 25 hard ReverseNodesinkGroup
  * 
  * Given a linked list, reverse the nodes of a linked list k at a time and
  * return its modified list.
@@ -25,13 +25,43 @@ package problems.hard;
  *
  */
 public class ReverseNodesinkGroup {
+
+	/**
+	 * 执行用时 : 1 ms , 在所有 java 提交中击败了 81.63% 的用户 内存消耗 : 38 MB , 在所有 java 提交中击败了
+	 * 82.83% 的用户
+	 * 
+	 * @param head
+	 * @param k
+	 * @return
+	 */
 	public ListNode reverseKGroup(ListNode head, int k) {
-
-		return null;
-	}
-
-	public void test() {
-		
+		ListNode resultHead = new ListNode(0);
+		ListNode revHead = resultHead, revTail = resultHead, tmp = null;
+		int i = 0;
+		for (; head != null; i = (++i) % k) {
+			tmp = head;
+			head = head.next;
+			if (i == 0) {
+				tmp.next = null;
+				revTail = revHead;
+				revHead.next = tmp;
+				revHead = revHead.next;
+			} else {
+				tmp.next = revTail.next;
+				revTail.next = tmp;
+			}
+		}
+		if (i != 0) {
+			revHead = revTail.next;
+			revTail.next = null;
+			while (revHead != null) {
+				tmp = revHead;
+				revHead = revHead.next;
+				tmp.next = revTail.next;
+				revTail.next = tmp;
+			}
+		}
+		return resultHead.next;
 	}
 
 	class ListNode {
@@ -41,5 +71,17 @@ public class ReverseNodesinkGroup {
 		ListNode(int x) {
 			val = x;
 		}
+	}
+
+	public static void main(String[] args) {
+		ReverseNodesinkGroup r = new ReverseNodesinkGroup();
+		ListNode n = r.new ListNode(1);
+		n.next = r.new ListNode(2);
+		n.next.next = r.new ListNode(3);
+		n.next.next.next = r.new ListNode(4);
+		n.next.next.next.next = r.new ListNode(5);
+		n.next.next.next.next.next = r.new ListNode(6);
+		n.next.next.next.next.next.next = r.new ListNode(7);
+		r.reverseKGroup(n, 5);
 	}
 }
