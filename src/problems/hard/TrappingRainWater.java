0a11ec48d1416825fa451f58ba7ef20932259571
zyuanyuz <zyuanyuz@outlook.com>
2020-01-02 18:44:09 +0800

    leetcode 42 TrappingRainWater Hard

--- src/problems/hard/TrappingRainWater.java
+++ src/problems/hard/TrappingRainWater.java
@@ -1,8 +1,10 @@
 package problems.hard;
-//leetcode 42
-//TODO fix bug
+
+//leetcode 42 TrappingRainWater Hard
 public class TrappingRainWater {
     /**
+     * 执行用时 : 1 ms , 在所有 java 提交中击败了 99.98% 的用户 内存消耗 : 37.7 MB , 在所有 java 提交中击败了
+     * 82.55% 的用户
      * 
      * @param height
      * @return
@@ -18,29 +20,37 @@ public class TrappingRainWater {
         }
         int resultCount = 0;
         // <-
-        int wallIndex = maxValueIndex;
-        for (int i = maxValueIndex - 1; i > 0;) {
-            for (int j = i; j > 0; j--) {
-                if (height[j] >= wallIndex) {
+        int wallIndex;
+        for (int i = maxValueIndex; i > 0;) {
+            wallIndex = i - 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (height[j] >= height[wallIndex]) {
                     wallIndex = j;
                 }
             }
-            for (int j = wallIndex + 1; j <= i; j++) {
+            for (int j = wallIndex + 1; j < i; j++) {
                 resultCount += (height[wallIndex] - height[j]);
             }
             i = wallIndex;
         }
         // ->
-        wallIndex = maxValueIndex;
-        for (int i = maxValueIndex + 1; i < height.length; i++) {
-            for (int j = i; j < height.length; i++) {
-
+        for (int i = maxValueIndex; i < height.length;) {
+            wallIndex = i + 1;
+            for (int j = i + 1; j < height.length; j++) {
+                if (height[j] >= height[wallIndex]) {
+                    wallIndex = j;
+                }
             }
-            for (int j = wallIndex - 1; j >= i; j--) {
+            for (int j = wallIndex - 1; j > i; j--) {
                 resultCount += (height[wallIndex] - height[j]);
             }
             i = wallIndex;
         }
         return resultCount;
+    }
+
+    public static void main(String[] args) {
+        TrappingRainWater t = new TrappingRainWater();
+        System.out.println(t.trap(new int[] { 4, 2, 0, 3, 2, 5 }));
     }
 }
