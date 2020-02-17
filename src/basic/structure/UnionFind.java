242281b4393f585a221f8278f951ac95d3326c8a
zyuanyuz <zyuanyuz@outlook.com>
2020-02-17 12:54:38 +0800

    UnionFind

--- src/basic/structure/UnionFind.java
+++ src/basic/structure/UnionFind.java
@@ -1,6 +1,46 @@
 package basic.structure;
 
-//TODO
-public class UnionFind{
-    
+public class UnionFind {
+    private int[] parent;
+    private int[] rank;
+
+    private UnionFind(int size) {
+        parent = new int[size];
+        rank = new int[size];
+    }
+
+    public static UnionFind init(int size) {
+        UnionFind unionFind = new UnionFind(size);
+        for (int i = 0; i < size; i++) {
+            unionFind.parent[i] = i;
+            unionFind.rank[i] = 0;
+        }
+        return unionFind;
+    }
+
+    public void unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y)
+            return;
+        if (rank[x] < rank[y]) {
+            parent[x] = y;
+        } else {
+            parent[y] = x;
+            if (rank[x] == rank[y])
+                rank[x]++;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        } else {
+            return (parent[x] = find(parent[x]));
+        }
+    }
+
+    public boolean same(int x, int y) {
+        return find(x) == find(y);
+    }
 }
---
+++ src/luogu/P2024.java
@@ -0,0 +1,8 @@
+package luogu;
+
+/**
+ * NOI2001
+ */
+public class P2024{
+
+}
