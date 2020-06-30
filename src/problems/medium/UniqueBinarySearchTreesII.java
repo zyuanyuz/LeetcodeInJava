48b6c460bf765870cc1f4eb1d40ca267568b87f8
zyuanyuz <zyuanyuz@outlook.com>
2020-06-30 20:35:41 +0800

    update

--- src/problems/medium/UniqueBinarySearchTreesII.java
+++ src/problems/medium/UniqueBinarySearchTreesII.java
@@ -4,38 +4,53 @@ import java.util.*;
 
 /**
  * leetcode 95 UniqueBinarySearchTreesII Medium
- * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
- * <p>
- * Example:
- * <p>
- * Input: 3
- * Output:
- * [
- * [1,null,3,2],
- * [3,2,null,1],
- * [3,1,null,null,2],
- * [2,1,3],
- * [1,null,2,null,3]
- * ]
- * Explanation:
- * The above output corresponds to the 5 unique BST's shown below:
- * <p>
- * 1         3     3      2      1
- * \       /     /      / \      \
- * 3     2     1      1   3      2
- * /     /       \                 \
- * 2     1         2                 3
  */
 public class UniqueBinarySearchTreesII {
 
     public List<TreeNode> generateTrees(int n) {
-        List<TreeNode> result = new ArrayList<>();
+        Map<Integer, NNodeTree> tMap = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
 
-        return null;
+        }
+        return tMap.get(n).rootNodeList;
     }
 
-    public void backtracking() {
+    class NNodeTree {
+        List<TreeNode> treeNodeList = new ArrayList<>();
+        List<TreeNode> rootNodeList = new ArrayList<>();
+        int minValue;
+        int n;
 
+        NNodeTree(int n, NNodeTree baseTree, Map<Integer, NNodeTree> tMap) {
+            if (baseTree == null) {
+                this.n = 1;
+                TreeNode node = new TreeNode(1);
+                this.treeNodeList.add(node);
+                this.treeNodeList.add(node);
+                this.minValue = 1;
+                tMap.put(1, this);
+            } else {
+                this.n = n;
+                for (int i = 0; i < n; i++) {
+                    TreeNode node = new TreeNode(n);
+                    if (i != 0) {
+                        tMap.get(i);
+                    }else{
+                        
+                    }
+                    if (n - i - 1 != 0) {
+                        tMap.get(n - i - 1).rootNodeList.get(1);
+                    }
+                }
+            }
+        }
+
+        public void allAdd(int value) {
+            minValue += value;
+            for (TreeNode node : treeNodeList) {
+                node.val += value;
+            }
+        }
     }
 
     class TreeNode {
