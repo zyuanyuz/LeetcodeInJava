b759ab73d19d15d097b5cea7887293237663a036
zyuanyuz <zyuanyuz@outlook.com>
2020-06-30 23:27:18 +0800

    update

---
+++ src/problems/hard/WordLadderII.java
@@ -0,0 +1,22 @@
+package problems.hard;
+
+import java.util.List;
+
+/**
+ * leetcode 126 WordLadderII
+ */
+
+public class WordLadderII {
+
+    /**
+     * 
+     */
+    int maxLength = Integer.MAX_VALUE;
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        boolean[] isReached = new boolean[wordList.size()];
+        
+        return null;
+    }
+
+}
--- src/problems/medium/UniqueBinarySearchTreesII.java
+++ src/problems/medium/UniqueBinarySearchTreesII.java
@@ -8,50 +8,12 @@ import java.util.*;
 public class UniqueBinarySearchTreesII {
 
     public List<TreeNode> generateTrees(int n) {
-        Map<Integer, NNodeTree> tMap = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-
-        }
-        return tMap.get(n).rootNodeList;
+        // TODO 
+        
+        return null;
     }
 
-    class NNodeTree {
-        List<TreeNode> treeNodeList = new ArrayList<>();
-        List<TreeNode> rootNodeList = new ArrayList<>();
-        int minValue;
-        int n;
-
-        NNodeTree(int n, NNodeTree baseTree, Map<Integer, NNodeTree> tMap) {
-            if (baseTree == null) {
-                this.n = 1;
-                TreeNode node = new TreeNode(1);
-                this.treeNodeList.add(node);
-                this.treeNodeList.add(node);
-                this.minValue = 1;
-                tMap.put(1, this);
-            } else {
-                this.n = n;
-                for (int i = 0; i < n; i++) {
-                    TreeNode node = new TreeNode(n);
-                    if (i != 0) {
-                        tMap.get(i);
-                    }else{
-                        
-                    }
-                    if (n - i - 1 != 0) {
-                        tMap.get(n - i - 1).rootNodeList.get(1);
-                    }
-                }
-            }
-        }
-
-        public void allAdd(int value) {
-            minValue += value;
-            for (TreeNode node : treeNodeList) {
-                node.val += value;
-            }
-        }
-    }
+    
 
     class TreeNode {
         int val;
