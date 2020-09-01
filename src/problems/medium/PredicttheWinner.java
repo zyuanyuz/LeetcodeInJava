ec4963047909b11e82fdb09df6b503d910815b76
zyuanyuz <zyuanyuz@outlook.com>
2020-09-01 16:31:30 +0800

    update

---
+++ src/problems/medium/PredicttheWinner.java
@@ -0,0 +1,20 @@
+package problems.medium;
+
+/**
+ * leetcode 486
+ */
+public class PredicttheWinner {
+    // TODO
+    public boolean PredictTheWinner(int[] nums) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PredicttheWinner p = new PredicttheWinner();
+        System.out.println(p.PredictTheWinner(
+                new int[] { 1, 2, 4, 5, 2, 324, 234, 234, 23, 4, 234, 23, 432, 4, 23, 42, 34, 23432, 423432, 4234, 23,
+                        423423, 423, 423423, 432432, 42, 34, 624, 5234, 23, 42, 3, 6, 234, 23, 523, 4, 234, 32, 42 }));
+        System.out.println(900L * 100 / 999L);
+    }
+}
